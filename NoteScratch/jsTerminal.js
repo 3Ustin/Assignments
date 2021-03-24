ad22f279f03716aa1465b5d620f05f284f6af41e
Austin Dupras <3ustin@gmail.com>
2021-03-24 13:05:21 -0400

    FLEX-ibleColumns

--- NoteScratch/jsTerminal.js
+++ NoteScratch/jsTerminal.js
@@ -1,2 +1,10 @@
-var x = 2;
-console.log(x);
+function countingPositivity(array){
+    var count = 0;
+    for(var i = 0; i < array.length; i++){
+        if(array[i] >= 0){
+            count++;
+        }
+    }
+    console.log(count);
+}
+countingPositivity([3,2,-1,5,16,8,-4]);
