439d51b7013b3bdca331927618c99b2f26829533
Austin Dupras <3ustin@gmail.com>
2021-03-26 14:16:04 -0400

    profile page edits

--- NoteScratch/jsTerminal.js
+++ NoteScratch/jsTerminal.js
@@ -8,3 +8,17 @@ function countingPositivity(array){
     console.log(count);
 }
 countingPositivity([3,2,-1,5,16,8,-4]);
+
+// function declaration
+function reverse(arr){     //arr = ["a","b","c","d","e"];
+    var temp; //temp stands for temparary
+    for(var i=0; i<arr.length/2;i++){ //the loop that loops through half of the array.
+        temp=arr[i]; //tempararily hold the first index in the array
+        arr[i]=arr[arr.length-1-i]; //makes the first index = to the last index
+        arr[arr.length-1-i]=temp; //makes the last index = the temparary value (which = the last index)
+    }
+    console.log(arr); //logs out the newly edited array.
+}
+reverse(["a","b","c","d","e"]);
+alphabet=["a","b","c","d","e"];
+console.log(alphabet.reverse());
