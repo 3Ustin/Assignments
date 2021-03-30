2ff15ce9675c5cbfb8435350ec72f9894f773965
Austin Dupras <3ustin@gmail.com>
2021-03-30 16:41:55 -0400

    updates

--- JS/DojoWeather/dojoWeather.js
+++ JS/DojoWeather/dojoWeather.js
@@ -2,29 +2,25 @@ function popUp(element){
     alert("We need to Load...");
 }
 function selectionClicking(){
-    var bodyLow = document.querySelectorAll(".bodyLow");
-    var bodyHigh = document.querySelectorAll(".bodyHigh");
-    console.log("bodyHigh: " + bodyHigh.length +" bodyLow: " + bodyLow.length);
-    if(document.getElementById("selection").selectedIndex == "1"){
-        for(var i = 0; i < bodyLow.length;i++){
-            bodyLow[i].innerText = parseInt((parseInt(bodyLow[i].innerText)*9/5) + 32);
-            bodyHigh[i].innerText =  parseInt((parseInt(bodyHigh[i].innerText)*9/5) + 32);
-
+    var bodyHLValue = document.querySelectorAll(".bodyHLValue");
+    //
+    //console.log("bodyHLValueLength: " + bodyHLValue.length +" bodyHLValue: " + bodyHLValue);
+    if(document.getElementById("selection").selectedIndex == "0"){
+        for(var i = 0; i < bodyHLValue.length;i++){
+            bodyHLValue[i].innerText =  parseInt((parseInt(bodyHLValue[i].innerText) - 32) * 5/9) + "°";
         }
         console.log("here");
         //(32°F − 32) × 5/9 = 0°C
     }
-    if(document.getElementById("selection").selectedIndex == "0"){
-        for(var i = 0; i < bodyLow.length;i++){
-            bodyLow[i].innerText = parseInt((parseInt(bodyLow[i].innerText) - 32) * 5/9);
-            bodyHigh[i].innerText =  parseInt((parseInt(bodyHigh[i].innerText) - 32) * 5/9);
-
+    if(document.getElementById("selection").selectedIndex == "1"){
+        for(var i = 0; i < bodyHLValue.length;i++){
+            bodyHLValue[i].innerText = parseInt((parseInt(bodyHLValue[i].innerText)*9/5) + 32) + "°";
         }
         console.log("here");
         //(32°F − 32) × 5/9 = 0°C
     }
 }
-
+// Go in and use .map() is a function for arrays
 function acceptQuest(element){
     element.parentElement.remove();
 }
